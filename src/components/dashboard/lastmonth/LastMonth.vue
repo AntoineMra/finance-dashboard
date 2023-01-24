89580ec7ebeffecace0c15625795a5f8df1463d7
Antoine Marionneau <58473024+AntoineMra@users.noreply.github.com>
2023-01-24 15:32:13 +0100

    Update LastMonth.vue

--- src/components/dashboard/lastmonth/LastMonth.vue
+++ src/components/dashboard/lastmonth/LastMonth.vue
@@ -6,8 +6,9 @@
 		</div>
 		<LastTransac />
 		<div class="py-2 px-8 flex items-center justify-end">
-			<router-link to="/budget/new" custom v-slot="{ navigate }">
-				<button @click="navigate" @keypress.enter="navigate" type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Créer son prochain budget</button>
+			// TODO: Dynamiser avec l'id pour obtenir le détail
+			<router-link to="/budget/{id}/recap" custom v-slot="{ navigate }">
+				<button @click="navigate" @keypress.enter="navigate" type="button" class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Découvrir le détail</button>
 			</router-link>
 		</div>
 	</section>
@@ -16,7 +17,6 @@
 <script setup lang="ts">
 import LastRecap from './LastRecap.vue';
 import LastTransac from './LastTransac.vue';
-
 </script>
 
 <style lang="scss" scoped>
