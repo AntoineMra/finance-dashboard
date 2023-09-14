79d16dce7103e4137b8d93d1141c7fb9c28da4ab
Antoine Marionneau <58473024+AntoineMra@users.noreply.github.com>
2023-09-14 16:34:46 +0200

    Format default Transactions page

--- src/views/budget/BudgetTransactionsView.vue
+++ src/views/budget/BudgetTransactionsView.vue
@@ -3,71 +3,23 @@
     <section class="glass py-8 pl-8 pb-16">
       <header>
         <h2 class="py-8 px-8 text-4xl font-bold flex-shrink-0">
-          Mon nouveau budget
+          J'ajoute mes transactions
         </h2>
-        <nav class="flex">
-          <router-link
-            :active-class="'isActive'"
-            to="/budget/new/income"
-            class="text-lg font-bold border-r-purple-500 p-2 mr-2 hover:bg-purple-200"
-            >Revenus</router-link
-          >
-          <router-link
-            :active-class="'isActive'"
-            to="/budget/new/expense"
-            class="text-lg font-bold p-2 ml-2 hover:bg-purple-200"
-            >Dépenses</router-link
-          >
-        </nav>
       </header>
 
       <div>
+
+        <!-- The form should add ligne to an existing transaction table -->
         <form
-          @submit.prevent="createBudget()"
+          @submit.prevent="createTransactions()"
           class="mx-auto mt-16 max-w-xl sm:mt-20"
         >
-          <div class="grid grid-cols-1 gap-y-6 gap-x-8 sm:grid-cols-2">
-            <div>
-              <label
-                for="name"
-                class="block text-sm font-semibold leading-6 text-gray-900"
-                >Nom du budget</label
-              >
-              <div class="mt-2.5">
-                <input
-                  v-model="title"
-                  type="text"
-                  name="name"
-                  id="name"
-                  autocomplete="Budget"
-                  class="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600"
-                />
-              </div>
-            </div>
-            <div class="sm:col-span-2">
-              <label
-                for="date"
-                class="block text-sm font-semibold leading-6 text-gray-900"
-                >Date</label
-              >
-              <div class="mt-2.5">
-                <input
-                  v-model="date"
-                  type="date"
-                  name="date"
-                  id="date"
-                  class="block w-full rounded-md border-0 py-2 px-3.5 text-sm leading-6 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-600"
-                />
-              </div>
-            </div>
-          </div>
-          <router-view />
           <div class="mt-10">
             <button
               type="submit"
               class="block w-full rounded-md bg-purple-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-purple-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-600"
             >
-              Suivant
+              Ajouter
             </button>
           </div>
         </form>
@@ -79,23 +31,38 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { getInstance } from "@/api/axios";
-import type { Budget } from "@/interface/api";
+import type { Transaction, Category } from "@/interface/api";
 
-const title = ref<string>("");
-const date = ref<Date>();
+const label = ref<string>("");
+const amount = ref<number | null>(null);
+const date = ref<Date | null>(null);
+const category = ref<string>('');
 
-const createBudget = async () => {
-  const budget: Partial<Budget> = {
+const createTransaction = async () => {
+  const id = route.params.id;
+  
+  const transaction: Partial<Transaction> = {
     title: title.value,
+    amount: amount.value,
     date: date.value,
-    status: "draft",
-    transactions: [],
+    category: category.value,
+    budget: `api/budgets/${id}`
   };
-  const instance = getInstance();
-  instance.post("/budget", budget);
 
-  //TODO should redirect to Budget Extraction View
+  // TODO : Add a Comment property to transactions in case of od or adjustments
+
+  const instance = getInstance();
+  instance.post("/transaction", transaction);
+
+  resetFieldsValues()
 };
+
+const resetFieldsValues = () => {
+  label.value = "";
+  amount.value = null;
+  date.value = null;
+  category.value = '';
+}
 
 </script>
 
