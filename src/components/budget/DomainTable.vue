ef4aa29e63d06ff5c7194f4040ead57071c69fb4
Antoine Marionneau <58473024+AntoineMra@users.noreply.github.com>
2023-10-16 16:12:28 +0200

    Add category table and domain to budget recap w/out values TBD

---
+++ src/components/budget/DomainTable.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="w-full -mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+    <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+      <table class="min-w-full leading-normal">
+        <thead>
+          <tr>
+            <th
+              class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-bold text-gray-600 uppercase"
+            >
+              Domaine
+            </th>
+            <th
+              class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-bold text-gray-600 uppercase"
+            >
+              Montant
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="domain in domains" :key="domain.id">
+            <td
+              class="px-5 py-5 border-b text-center border-x-2 border-gray-200 bg-white text-sm"
+            >
+              {{ domain.name }}
+            </td>
+            <td
+              class="px-5 py-5 border-b text-center border-x-2 border-gray-200 bg-white text-sm"
+            >
+              {{ getDomainValue(domain.id) }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Budget, Domain } from "@/interface/api";
+import { onBeforeMount, ref } from "vue";
+import { useCategoryStore } from "@/stores/category";
+import { getInstance } from "@/api/axios";
+
+const props = defineProps<{
+  budget: Buget | null;
+}>();
+
+let domains = ref<Domain[]>([]);
+
+const getDomains = async () => {
+  const instance = getInstance();
+
+  try {
+    const response = await instance.get("/domains");
+
+    domains.value = response.data;
+  } catch (error: any) {
+    if (error.response.status === 401) handleExpiredToken();
+  }
+};
+
+onBeforeMount(() => {
+  getDomains();
+});
+</script>
+
+<style scoped></style>
